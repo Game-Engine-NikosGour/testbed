afaea6eeda576860970975e1c73576733a8fc7d7
NikosGour <nikosgournak@gmail.com>
2025-04-02 13:00:06 +0300

    chore: nothing changed

--- src/main.go
+++ src/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	game_lib "github.com/NikosGour/Game-Engine/src/lib"
 	"github.com/NikosGour/testbed/src/build"
 )
 
-func main(){
-	fmt.Printf("DEBUG_MODE = %t\n",build.DEBUG_MODE)
+func main() {
+	fmt.Printf("DEBUG_MODE = %t\n", build.DEBUG_MODE)
+	game_lib.Add(2, 3)
+
 }
